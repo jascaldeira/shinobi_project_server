d63e81a66711ee9f6c39a5ac146d7c7d91365715
Vincent Stumpf <vincents.995@gmail.com>
2023-03-20 09:32:41 -0700

    Fix ASAN crashes and LSAN leaks (#7649)

--- src/map/unit.cpp
+++ src/map/unit.cpp
@@ -3509,6 +3509,7 @@ int unit_free(struct block_list *bl, clr_type clrtype)
 
 			skill_clear_unitgroup(bl);
 			status_change_clear(bl,1);
+			pd->~pet_data();
 			break;
 		}
 		case BL_MOB: {
@@ -3544,6 +3545,7 @@ int unit_free(struct block_list *bl, clr_type clrtype)
 						gc->temp_guardians[i] = 0;
 				}
 
+				md->guardian_data->~guardian_data();
 				aFree(md->guardian_data);
 				md->guardian_data = NULL;
 			}
@@ -3572,6 +3574,7 @@ int unit_free(struct block_list *bl, clr_type clrtype)
 
 			if( md->tomb_nid )
 				mvptomb_destroy(md);
+			md->~mob_data();
 			break;
 		}
 		case BL_HOM:
